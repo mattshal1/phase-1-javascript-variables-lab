34484d744e9b1014ef4fba5a7d199dde0c083820
Ahmed Shalash <ah.shalash@gmail.com>
2023-10-12 16:22:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Declare and initialize companyName as a const
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood with let (set it to 'Chelsea')
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo with let (set it to 'Susan Smith')
+let companyCeo = 'Susan Smith';
